4878d2a5d4324c5c5c1bf3179c4e7d590e06d499
Adrien Varricchione <47208777+Shedhatch@users.noreply.github.com>
2022-06-20 16:27:08 +0200

    Update Timer.js
    - Improved messages from some return
    - Formatted code

--- Timer/Timer.js
+++ Timer/Timer.js
@@ -2,22 +2,16 @@ const Discord = module.require('discord.js');
 const ms = require('ms');
 
 module.exports.run = async (bot, message, args) => {
-
   let Timer = args[0];
-
   if(!args[0]){
     return message.channel.send("Error :x: " + "| Please Enter a time period followed by \"s or m or h\"");
   }
-
   if(args[0] <= 0){
-    return message.channel.send("Error :x: " + "| Please Enter a time period followed by \"s or m or h\"");
+    return message.channel.send("Error :x: " + "| Please Enter a valid time period followed by \"s or m or h\"");
   }
-
   message.channel.send("⏲️" + "| Timer Started for: " + `${ms(ms(Timer), {long: true})}`)
-
   setTimeout(function(){
-    message.channel.send(message.author.toString() + ` The Timer Has FINISHED!, it lasted: ${ms(ms(Timer), {long: true})}`)
-
+    message.channel.send(message.author.toString() + ` The Timer Has finished!, it lasted for: ${ms(ms(Timer), {long: true})}`)
   }, ms(Timer));
 }
 
